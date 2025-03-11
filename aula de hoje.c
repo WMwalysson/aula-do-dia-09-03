fb48dcc980240087ed4c74c038c55a9b4202f642
WMwalysson <202502092041@alunos.estacio.br>
2025-03-11 13:28:19 +0000

    aula 11/03

--- aula de hoje.c
+++ aula de hoje.c
@@ -2,20 +2,22 @@
 
 int main(){
 
-int i, j;
-char letra;
-for ( i = 1; i <= 5; i++)
-{
-    letra = 'A';
-    for (j = 1 ; j <= i; j++)
-    {
-        printf("%c ", letra);
-        ++letra;
-    }
-    printf("\n");
+int index;
 
-    }
+char * nomesAlunos[3][3] ={
+    {"Aluno 0", "Pt: 30", "Mat: 90"},
+    {"Aluno 1", "Pt: 60", "Mat: 60"},
+    {"Aluno 2", "Pt: 90", "Mat: 30"}
+};
+  printf("Digite o numero do aluno que queira ver as notas...\n");
+  printf("Para o aluno 0, digite 0 \n");
+  printf("Para o aluno 1, digite 1 \n");
+  printf("Para o aluno 2, digite 2 \n");
 
+  scanf ("%d", &index);
+  printf("As notas do %s são: %s , %s...\n", nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2]);
+
+    
     return 0;
 
 }
